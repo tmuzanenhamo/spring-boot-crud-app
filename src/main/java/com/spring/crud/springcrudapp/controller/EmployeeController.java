755672f6d8f980cfb48850bd9623f0e8219638a2
Tawanda <tawandamuzanenhamo14@gmail.com>
2021-12-29 13:13:44 +0200

    add get, post and get by id rest endpoints

---
+++ src/main/java/com/spring/crud/springcrudapp/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.spring.crud.springcrudapp.controller;
+
+import java.util.List;
+
+import com.spring.crud.springcrudapp.model.Employee;
+import com.spring.crud.springcrudapp.service.EmployeeService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // The annotation provides the @Controller Annotarion and @ResponseBody annotation 
+// Base URL
+@RequestMapping("api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        super();
+        this.employeeService = employeeService;
+    }
+
+    // Create Employee REST API
+    @PostMapping
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    // Get All Employees REST API
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    // Get Employee by id
+    // @Path variable anotation extracts the id from the url
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeByID(@PathVariable("id") long id) {
+        return new ResponseEntity<Employee>(employeeService.getEmployeeByID(id), HttpStatus.OK);
+    }
+
+}
