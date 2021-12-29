8ff4650902b39c21f1c4440124fdebde979c87cc
Tawanda <tawandamuzanenhamo14@gmail.com>
2021-12-29 17:45:14 +0200

    add put, delete rest apis

--- src/main/java/com/spring/crud/springcrudapp/controller/EmployeeController.java
+++ src/main/java/com/spring/crud/springcrudapp/controller/EmployeeController.java
@@ -7,9 +7,11 @@ import com.spring.crud.springcrudapp.service.EmployeeService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,19 @@ public class EmployeeController {
     @GetMapping("{id}")
     public ResponseEntity<Employee> getEmployeeByID(@PathVariable("id") long id) {
         return new ResponseEntity<Employee>(employeeService.getEmployeeByID(id), HttpStatus.OK);
+    }
+
+    // Update Employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id, @RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+    }
+
+    // Delete Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id) {
+        employeeService.deleteEmployee(id);
+    return new ResponseEntity<String>("Employee Deleted Successfully!", HttpStatus.OK);
     }
 
 }
